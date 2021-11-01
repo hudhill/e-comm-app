08be8f79d59b49a1d4cc5d710a089dcfcb897ffd
Sam Hill <sam.hudhill@gmail.com>
2021-11-01 11:37:51 +0000

    imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 var element = <div>Hello</div>;
 ReactDOM.render(element, document.getElementById("root"));
