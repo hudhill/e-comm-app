3894107045a52b49be81684a75d842e3b174617b
Sam Hill <sam.hudhill@gmail.com>
2021-11-01 12:18:50 +0000

    installed packages and imported

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+class NavBar extends Component{
+  render(){
+    return (
+			<div>
+				<h1>Hello</h1>
+				<p>paragraph</p>
+			</div>
+		);
+  }
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App";
 
-var element = <button class="btn btn-danger">Hello World</button>;
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<NavBar />, document.getElementById("root"));
