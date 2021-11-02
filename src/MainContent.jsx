ff9f74f52fc458381a7954df0db959929d2a6c8c
Sam Hill <sam.hudhill@gmail.com>
2021-11-02 12:24:25 +0000

    Refresh

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,14 +1,22 @@
 import React, {Component} from "react";
 
 class MainContent extends Component {
-  state = {appTitle:"Customers"};
+  state = {pageTitle:"Customers", customerCount:5};
   render(){
     return (
       <div>
-        <h4>{this.state.appTitle}</h4>
+        <h4 className="border-bottom m-1 p-1">{this.state.pageTitle}
+          <span className="badge badge-secondary m-2">{this.state.customerCount}</span>
+          <button className="btn btn-info" onClick={this.onRefreshClick}>Refresh</button>
+        </h4>
       </div>
     );
   }
+
+  onRefreshClick(){
+    console.log("Refreshclicked");
+  }
+
 }
 
 export default MainContent
